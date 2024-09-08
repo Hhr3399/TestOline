a35a9919cf8070f8d1379eb66efecce41f41eff1
asus <1619392038@qq.com>
2024-09-08 14:47:12 +0800

    Initial commit

---
+++ src/main/java/com/itheima/testoline/TestOlineApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.testoline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestOlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itheima/testoline/TestOlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.testoline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
